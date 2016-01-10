754efa86a788639afca8b4543077ef5d82881a29
Johan Henriksson <johanhenriksson@live.com>
2016-01-10 07:35:24 +0100

    memory write/protect abstractions

--- memory.c
+++ memory.c
@@ -5,29 +5,87 @@
 #include <mach/error.h>
 #include <mach/mach_vm.h>
 
-mach_vm_address_t remote_alloc(task_t task, size_t size) {
-    mach_vm_address_t addr;
+vm_mem_t* vm_mem_alloc(
+    task_t task, 
+    size_t size) 
+{
+    vm_mem_t* mem = malloc(sizeof(vm_mem_t));
+    mem->task = task;
+    mem->size = size;
+
     if (mach_vm_allocate(task, 
-            &addr, 
+            &mem->addr, 
             size, 
             VM_FLAGS_ANYWHERE) != KERN_SUCCESS) 
     {
         panic("Unable to allocate VM memory\n");
     }
-    return addr;
+
+    return mem;
 }
 
-void remote_write(
-    task_t task, 
-    mach_vm_address_t offset,
-    void* ptr, 
+void vm_mem_free(vm_mem_t* mem) {
+    // TODO: find out how to deallocate vm memory
+    free(mem);
+}
+
+void* vm_mem_read(
+    vm_mem_t* mem,
+    void* offset,
+    size_t count)
+{
+    uint32_t size;
+    void* vm_ptr;
+    if (vm_read(
+            mem->task, 
+            (vm_offset_t)offset, 
+            count, 
+            (vm_offset_t*)&vm_ptr, 
+            &size) != KERN_SUCCESS) {
+        panic("vm_read failed\n");
+    }
+
+    uint8_t* buffer = malloc(size);
+    if (buffer == NULL) {
+        panic("malloc failed\n");
+    }
+
+    memcpy(buffer, vm_ptr, size);
+    return buffer;
+}
+
+void vm_mem_write(
+    vm_mem_t* mem, 
+    void* dest,
+    void* source, 
     size_t count) 
 {
-    if (vm_write(task, 
-            offset, 
-            (vm_offset_t)ptr, 
+    if (vm_write(mem->task, 
+            (mach_vm_address_t)dest, 
+            (vm_offset_t)source, 
             count) != KERN_SUCCESS)
     {
         panic("vm_write failed\n");
     }
 }
+
+void vm_mem_mark_execute(vm_mem_t* mem) {
+    if (vm_protect(mem->task, 
+            mem->addr, 
+            mem->size, 
+            FALSE, 
+            VM_PROT_READ | VM_PROT_EXECUTE) != KERN_SUCCESS) {
+        panic("vm_protect (executable) failed");
+    }
+}
+
+void vm_mem_mark_write(vm_mem_t* mem) {
+    /* Mark stack as writable  - not really necessary */
+    if (vm_protect(mem->task, 
+            mem->addr, 
+            mem->size, 
+            TRUE, 
+            VM_PROT_READ | VM_PROT_WRITE) != KERN_SUCCESS) {
+        panic("vm_protect (writable) failed");
+    }
+
--- memory.h
+++ memory.h
@@ -1,9 +1,23 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#include <mach/mach.h>
+#include <mach/mach_vm.h>
+
+typedef struct vm_mem_t vm_mem_t;
+
 struct vm_mem_t {
+    task_t task;
     mach_vm_address_t addr; // remote copy
-    void* local; // local copy
+    size_t size;
 };
+
+vm_mem_t* vm_mem_alloc(task_t, size_t);
+void vm_mem_free(vm_mem_t*);
+
+void vm_mem_write(vm_mem_t*, void*, void*, size_t);
+void* vm_mem_read(vm_mem_t*, void*, size_t);
+void vm_mem_mark_execute(vm_mem_t* mem) {
+void vm_mem_mark_write(vm_mem_t* mem) {
 
 #endif
